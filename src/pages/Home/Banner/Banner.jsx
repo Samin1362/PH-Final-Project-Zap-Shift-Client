2704956f95790752678b7de6a48d8664af9bbdd9
Md. Samin Israk 2021362642 <samin.israk@northsouth.edu>
2025-11-18 08:57:11 +0600

    banner relative and absolute added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -8,17 +8,35 @@ import bannerImg3 from "../../../assets/banner/banner3.png";
 const Banner = () => {
   return (
     <Carousel autoPlay={true} infiniteLoop={true}>
-      <div>
+      <div className="relative">
         <img src={bannerImg1} />
+        <div className="absolute bottom-10 left-10 flex gap-4">
+          <button className="btn btn-primary rounded-full text-black">
+            Track Your Parcel
+          </button>
+          <button className="btn">Be A Rider</button>
+        </div>
       </div>
-      <div>
+      <div className="relative">
         <img src={bannerImg2} />
+        <div className="absolute bottom-10 left-10 flex gap-4">
+          <button className="btn btn-primary rounded-full text-black">
+            Track Your Parcel
+          </button>
+          <button className="btn">Be A Rider</button>
+        </div>
       </div>
-      <div>
+      <div className="relative">
         <img src={bannerImg3} />
+        <div className="absolute bottom-10 left-10 flex gap-4">
+          <button className="btn btn-primary rounded-full text-black">
+            Track Your Parcel
+          </button>
+          <button className="btn">Be A Rider</button>
+        </div>
       </div>
     </Carousel>
-  ); 
+  );
 };
 
 export default Banner;
