200c123363b53c9a66d2035df703898b66467253
Md. Samin Israk 2021362642 <samin.israk@northsouth.edu>
2025-11-19 08:42:39 +0600

    react hook form added for registration page

--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -2,16 +2,59 @@ import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useLoaderData } from "react-router";
+import { useRef } from "react";
 
 const Coverage = () => {
   const serviceCenters = useLoaderData();
-  console.log(serviceCenters);
+  const mapRef = useRef(null);
 
   const position = [23.685, 90.3563];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const location = e.target.location.value;
+    const district = serviceCenters.find((c) =>
+      c.district.toLowerCase().includes(location.toLowerCase())
+    );
+    if (district) {
+      const coord = [district.latitude, district.longitude];
+      mapRef.current.flyTo(coord, 14);
+    }
+  };
 
   return (
     <div>
       <h1>We are available in 64 Districts</h1>
+
+      {/* search bar form */}
+      <div>
+        <form onSubmit={handleSearch}>
+          <label className="input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input
+              type="search"
+              className="grow"
+              name="location"
+              placeholder="Search"
+            />
+          </label>
+        </form>
+      </div>
 
       <div className="border w-full h-[800px]">
         <MapContainer
@@ -19,6 +62,7 @@ const Coverage = () => {
           center={position}
           zoom={13}
           scrollWheelZoom={false}
+          ref={mapRef}
         >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -26,11 +70,16 @@ const Coverage = () => {
           />
 
           {serviceCenters.map((center) => {
-            <Marker position={[center.latitude, center.longitude]}>
-              <Popup>
-                <strong>{center.district}</strong> <br /> Easily customizable.
-              </Popup>
-            </Marker>;
+            return (
+              <Marker
+                key={center.district}
+                position={[center.latitude, center.longitude]}
+              >
+                <Popup>
+                  <strong>{center.district}</strong> <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            );
           })}
         </MapContainer>
       </div>
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter } from "react-router";
 import Root from "../layouts/Root";
 import Home from "../pages/Home/Home/Home";
 import Coverage from "../pages/Coverage/Coverage";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Auth/Login/Login";
+import Register from "../pages/Auth/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +22,20 @@ const router = createBrowserRouter([
       }
     ]
   },
+  {
+    path: "/",
+    Component: AuthLayout, 
+    children: [
+      {
+        path: '/login', 
+        Component: Login
+      }, 
+      {
+        path: '/register', 
+        Component: Register
+      }
+    ]
+  }
 ]);
 
 export default router;
